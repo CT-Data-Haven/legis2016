6d4ad319d4c70aba8877441de7d4ec82f33ceca8
camille-s <camille.seaberry@gmail.com>
2018-01-05 11:26:44 -0500

    Add download link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => (
 	<div className="Footer">
 		<p>Source: DataHaven analysis (2017). US Census Bureau. American Community Survey 2016 5-year estimates.</p>
 		<p>Note: Names of senators and representatives current as of January 2, 2018.</p>
+		<p><a href="https://github.com/camille-s/legis2016/blob/master/public/data/2016%20legislative%20profiles.csv">Download data on all districts</a></p>
 	</div>
 );
 
