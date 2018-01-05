e844ad104e5882111c98f2694158bec28818d3c8
camille-s <camille.seaberry@gmail.com>
2018-01-05 10:52:10 -0500

    Add note on date of legislator names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import '../styles/Footer.css';
 
 const Footer = () => (
 	<div className="Footer">
-		<p>Sources: DataHaven analysis (2017). US Census Bureau. American Community Survey 2016 5-year estimates.</p>
+		<p>Source: DataHaven analysis (2017). US Census Bureau. American Community Survey 2016 5-year estimates.</p>
+		<p>Note: Names of senators and representatives current as of January 2, 2018.</p>
 	</div>
 );
 
