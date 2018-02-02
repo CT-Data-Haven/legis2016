af785a1c9dffe4bb18281cc90a360b49eac27fab
camille-s <camille.seaberry@gmail.com>
2018-02-02 13:07:51 -0500

    Add logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Segment, Image } from 'semantic-ui-react';
+
+import src from '../img/25th-logotype.jpg';
+
 import '../styles/Footer.css';
 
 const Footer = (props) => {
@@ -6,10 +10,12 @@ const Footer = (props) => {
 	let url = `https://github.com/CT-Data-Haven/legislative_handouts/blob/public/handout/output/2016_profile_${props.chamber}_${id}.pdf`;
 	return (
 		<div className="Footer">
-			<p><a href={url} target="_blank">Download printable profile on this district (pdf)</a> or <a href="https://github.com/ct-data-haven/legis2016/blob/master/public/data/2016_legislative_profiles.csv" target="_blank">data on all districts</a></p>
-			<p>Source: DataHaven analysis (2017). US Census Bureau. American Community Survey 2016 5-year estimates; and <a href="http://ctdatahaven.org/reports/datahaven-community-wellbeing-survey" target="_blank">2015 DataHaven Community Wellbeing Survey</a>.</p>
-			<p>Note: Names of senators and representatives current as of January 2, 2018.</p>
-			<p> </p>
+			<Segment secondary basic>
+				<p><a href={url} target="_blank">Download printable profile on this district (pdf)</a> or <a href="https://github.com/ct-data-haven/legis2016/blob/master/public/data/2016_legislative_profiles.csv" target="_blank">data on all districts</a></p>
+				<p>Source: DataHaven analysis (2017). US Census Bureau. American Community Survey 2016 5-year estimates; and <a href="http://ctdatahaven.org/reports/datahaven-community-wellbeing-survey" target="_blank">2015 DataHaven Community Wellbeing Survey</a>.</p>
+				<p>Note: Names of senators and representatives current as of January 2, 2018.</p>
+				<Image src={src} size="small" as="a" href="http://www.ctdatahaven.org" target="_blank" />
+			</Segment>
 		</div>
 	);
 };
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,6 +8,8 @@ const Intro = () => (
 		<p>Clicking a district on the map brings up detailed information on that district and its legislator.</p>
 
 		<p>Community Wellbeing Survey data is available only for Senate districts; Census data is available for both Senate and House districts.</p>
+
+		<p>For more information on Connecticut's communities and cities, visit DataHaven's <a href="http://www.ctdatahaven.org/communities">Communities</a> page or <a href="http://www.ctdatahaven.org">main website</a>.</p>
 	</Message>
 );
 
